bd0377fd80d0e1edfaa6297a04aca0c9362997d0
Denjiestt <-v>
2023-04-04 22:57:23 +0700

    test 14

--- config/Database.js
+++ config/Database.js
@@ -3,14 +3,18 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-// const db = new Sequelize(process.env.DATABASE,process.env.DB_USERNAME,process.env.DB_PASSWORD, {
-//     host: process.env.DB_HOST,
-//     dialect : process.env.DB_DIAsLECT
-// })
-
-const db = new Sequelize(process.env.DB_URI, {
-    define:{
-        timestamps:false
-    }
+const db = new Sequelize({
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    dialect : "mysql"
 })
+
+// const db = new Sequelize(process.env.DB_URI, {
+//     define:{
+//         timestamps:false
+//     }
+// })
 export default db
