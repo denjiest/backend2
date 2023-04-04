c2cdc7e4ba3c1eaa96e7ff83180995ce01217727
Denjiestt <-v>
2023-04-04 12:37:52 +0700

    upload 2

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', (req,res)=>{
     console.log("hello world")
 })
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+  console.log("Server is listening on port "+port+"");
 });
