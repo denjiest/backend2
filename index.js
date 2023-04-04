11da46f4c2788c14585b24a539a0605f7d6b4687
Denjiestt <-v>
2023-04-04 12:28:26 +0700

    coba backend pertama

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express"
+import cors from "cors"
+
+const app = express();
+
+app.use(cors())
+
+app.get('/', (req,res)=>{
+    console.log("hello world")
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
