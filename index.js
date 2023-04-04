a77e7dfd0a1b47456eb3e7a5fba8b7cd43c125f3
Denjiestt <-v>
2023-04-04 12:44:24 +0700

    test 3

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ const app = express();
 
 app.use(cors())
 
-app.get('/', (req,res)=>{
-    console.log("hello world")
-})
-
 const port = 3000;
 app.listen(port, () => {
   console.log("Server is listening on port "+port+"");
