25d2dc419ead1438470b97497d553c9fc6e6d7bd
Denjiestt <-v>
2023-04-04 13:44:53 +0700

    test 8

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express"
+import cors from "cors"
 import dotenv from "dotenv"
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req,res)=>{
     res.send('welcome to the new e')
@@ -14,6 +16,13 @@ app.get('/', (req,res)=>{
 app.get('/local', (req,res)=>{
     res.send('berjalan di localhost')
 })
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
 
 const port = process.env.PORT || 3000;
 
