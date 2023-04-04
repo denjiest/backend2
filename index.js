61d2e69b84c3cee9c7511d1da1e105a80dd1861b
Denjiestt <-v>
2023-04-04 13:10:04 +0700

    test 4

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 import express from "express"
-import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const app = express();
 
-app.use(cors())
 app.use(express.json())
 
 app.get('/', (req,res)=>{
     res.send('welcome to the new e')
 })
 
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log("Server is listening on port "+port+"");
 });
